9e9efb31c8d16d1b70fc1ef12bf3721b0fbd278b
compactvision <heymytechcorporation@gmail.com>
2025-11-29 16:05:19 +0100

    Update favicon logo

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -5,10 +5,12 @@ import Products from "@/components/section/Products";
 import Values from "@/components/section/Values";
 import Testimonials from "@/components/section/Testimonials";
 import Contact from "@/components/section/Contact";
+import { Head } from "@inertiajs/react";
 
 export default function Welcome() {
     return (
         <AppShell>
+            <Head title="Accueil" />
             <Hero />
             <About />
             <Products />
